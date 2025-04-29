db5469952560d2e08f2751288fa89e1b10ce6f21
brunoaaraujo <bruno.alvarenga2@gmail.com>
2025-04-28 21:49:47 -0300

    Acrescimo de paragrafo para inclusão de valor digitado no input do index.html, estilização desdes paragrafos para configurações ja existentes no style.css e feito a interação do script.js para capturar o que e digitado no input e colocar no primeiro paragrado.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.querySelector('#first-coin').addEventListener('change' , function(){
     const imageSelect1 = this.value;
-    const textoSelect = this.options[this.selectedIndex].dataset.texto;
-
     const imageToDisplay1 =  document.querySelector('#main-currency-image');
-    imageToDisplay1.src = imageSelect1;
-
+    const textoSelect = this.options[this.selectedIndex].dataset.texto;
     const paragraphToUpdate = document.querySelector('#text-coin');
+    const textoSelect3 = this.options[this.selectedIndex].dataset.valor;
+    const paragraphToUpdate3 = document.querySelector('#coin-symbol1');
+    
+    
+    imageToDisplay1.src = imageSelect1;
     paragraphToUpdate.textContent = textoSelect;
+    paragraphToUpdate3.textContent = textoSelect3;
 })
 
 
@@ -14,11 +17,22 @@ document.querySelector('#first-coin').addEventListener('change' , function(){
 document.querySelector('#second-coin').addEventListener('change', function(){
     const imageSelect2 = this.value;
     const imageToDisplay2 = document.querySelector('#second-currency-image');
-    
     const textoSelect2 = this.options[this.selectedIndex].dataset.texto;
-    imageToDisplay2.src = imageSelect2;
-
     const paragraphToUpdate2 = document.querySelector('#text-coin2');
+    const textoSelect4 = this.options[this.selectedIndex].dataset.valor;
+    const paragraphToUpdate4 = document.querySelector('#coin-symbol2');
+    
+    
+    imageToDisplay2.src = imageSelect2;
     paragraphToUpdate2.textContent = textoSelect2;
+    paragraphToUpdate4.textContent = textoSelect4;
 })
+
+document.querySelector('#value-input').addEventListener('input', function(){
+    const inputValue = this.value;
+    const textoSelect5 = document.querySelector('#coin-value');
+
+    textoSelect5.textContent = inputValue;
+})
+
 
