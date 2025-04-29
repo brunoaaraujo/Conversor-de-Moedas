d70cb86e75dc7ba918a8871e5f865054dadfeed1
brunoaaraujo <bruno.alvarenga2@gmail.com>
2025-04-28 21:02:33 -0300

    Atualização de id="text-coin" e atualização de texto do paragrafo de acorco com a seleção do option na tag select.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 document.querySelector('#first-coin').addEventListener('change' , function(){
     const imageSelect1 = this.value;
-    const imageToDisplay1 =  document.querySelector('#main-currency-image');
+    const textoSelect = this.options[this.selectedIndex].dataset.texto;
 
+    const imageToDisplay1 =  document.querySelector('#main-currency-image');
     imageToDisplay1.src = imageSelect1;
+
+    const paragraphToUpdate = document.querySelector('#text-coin');
+    paragraphToUpdate.textContent = textoSelect;
 })
+
+
 
 document.querySelector('#second-coin').addEventListener('change', function(){
     const imageSelect2 = this.value;
     const imageToDisplay2 = document.querySelector('#second-currency-image');
-
+    
+    const textoSelect2 = this.options[this.selectedIndex].dataset.texto;
     imageToDisplay2.src = imageSelect2;
+
+    const paragraphToUpdate2 = document.querySelector('#text-coin2');
+    paragraphToUpdate2.textContent = textoSelect2;
 })
+
